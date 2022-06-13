3f2653d72ff432dc496cd7f8a25002fba9de5acf
Aaron Johncock <aaron.johncock@hotmail.co.uk>
2022-06-13 19:44:27 +0100

    Initial Commit

---
+++ TravelScrapbook/TravelScrapbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScrapbookApp.swift
+//  TravelScrapbook
+//
+//  Created by Aaron Johncock on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelScrapbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
